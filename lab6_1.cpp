54be9d51efaae4c4eadc32ec007f4bc22ba7c7d7
LimPrattana <vivo.twy0270@gmail.com>
2024-11-29 15:55:53 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+int num[100],even=0,odd=0;
+int i=0;
+cout << "Enter an integer: ";
+    cin >> num[0];
+while (num[i]!=0)
+{   if(num[i]%2==0){
+    even ++;
+}
+else{
+    odd ++;
+}
+    i++;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num[i];
+    
+}
+
+    
+
+    cout << "#Even numbers = "<<even<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
